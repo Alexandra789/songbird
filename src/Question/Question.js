539898df37b4645ac7c017f0442a8126fe0a5ce7
unknown <zakovinko99@gmail.com>
2021-01-27 19:31:23 +0300

    add question component

--- src/Question/Question.js
+++ src/Question/Question.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import './Question.css';
-// import logo from './logo.png';
+import birdShadow from './bird-shadow.jpg';
 
 export default function Question() {
+    const [name, setName] = useState('******');
+    const [img, setImg] = useState({ birdShadow });
     return (
         <div className="question-container">
-
-        </div>
+            <div className="bird-random">
+            <img src={birdShadow} alt="bird shadow" />
+            <div className="question-content">
+                <div className="name">{name}</div>
+            </div>
+        </div></div>
     )
 }
