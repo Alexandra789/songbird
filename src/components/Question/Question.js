8d4388584f6d0070ce5fc050ba15316e529904ae
unknown <zakovinko99@gmail.com>
2021-01-28 20:17:36 +0300

    add player

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -4,6 +4,7 @@ import birdShadow from './bird-shadow.jpg';
 export default function Question(props) {
     return (
         <div className="question-container">
+
             {props.displayImage ?
                 (<img src={props.displayImage} alt="bird shadow" />)
                 :
@@ -20,6 +21,10 @@ export default function Question(props) {
                     :
                     ''
                 }
+                <audio className="bird-voice" controls preload="metadata">
+                    <source src={"https://www.xeno-canto.org/sounds/uploaded/XIQVMQVUPP/XC518684-Grands%20corbeaux%2009012020%20Suzon.mp3"} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                </audio>
             </div>
         </div>
     )
